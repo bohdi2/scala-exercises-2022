dd8966bed0fe4b331674f3a4c5eab56c9f58ceec
Christopher Helck <christopher.helck@gmail.com>
2022-01-05 14:54:47 -0500

    CJH cleanup heap

--- Heap/src/test/scala/core/HeapTests.scala
+++ Heap/src/test/scala/core/HeapTests.scala
@@ -2,6 +2,7 @@ package core
 
 import org.scalatest.funsuite.AnyFunSuite
 
+import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
 
 object HeapTests {
@@ -12,36 +13,37 @@ object HeapTests {
   }
 
 
-  class Heap(val originalData: Seq[Int]) {
-    private val data = ArrayBuffer(originalData: _*)
+  class Heap(private val originalData: Seq[Int]) {
+    private val array = ArrayBuffer(originalData: _*)
 
-    def size: Int = data.size
-    def apply(index: Int): Int = data(index)
+    def size: Int = array.size
+    def apply(index: Int): Int = array(index)
     def top: Int = apply(0)
     def isRoot(index: Int): Boolean = index == 0
     def calcParentIndex(childIndex: Int): Int = (childIndex - 1) / 2
     def calcLeftChild(parentIndex: Int): Int = parentIndex*2 + 1
-    def rightChild(parentIndex: Int): Int = parentIndex*2 + 2
+    def calcRightChild(parentIndex: Int): Int = parentIndex*2 + 2
 
 
-    def update(index: Int, value: Int): Unit = data.update(index, value)
+    def update(index: Int, value: Int): Unit = array.update(index, value)
 
     def insert(value: Int): Heap = {
-      val childIndex = data.size
-      data.append(value)
+      val childIndex = size
+      array.append(value)
 
       val holeIndex = siftUp(childIndex)
-      data.update(holeIndex, value)
+      array.update(holeIndex, value)
       this
     }
 
-    def siftUp(childIndex: Int): Int = {
+    @tailrec
+    private def siftUp(childIndex: Int): Int = {
       if (isRoot(childIndex)) {
         return childIndex
       }
 
       val parentIndex = calcParentIndex(childIndex)
-      if (data(parentIndex) >= data(childIndex)) {
+      if (array(parentIndex) >= array(childIndex)) {
         childIndex
       }
       else if (swap(parentIndex, childIndex))
@@ -51,15 +53,15 @@ object HeapTests {
     }
 
     def removeTop(): Heap = {
-      update(0, data.remove(data.size - 1))
-      data.trimToSize()
+      update(0, array.remove(size - 1))
+      array.trimToSize()
       siftDown(0)
       this
     }
 
-    def siftDown(parentIndex: Int): Heap = {
+    private def siftDown(parentIndex: Int): Heap = {
       val leftChildIndex = calcLeftChild(parentIndex)
-      val rightChildIndex = rightChild(parentIndex)
+      val rightChildIndex = calcRightChild(parentIndex)
       //println(s"CJH siftDown ${this}, ${x(parentIndex)} -> ${x(leftChildIndex)}, ${x(rightChildIndex)}")
 
       if (size - 1 == parentIndex) {
@@ -67,7 +69,7 @@ object HeapTests {
       else if (size - 1 == leftChildIndex) {
         swap(parentIndex, leftChildIndex)
       }
-      else if (data(leftChildIndex) < data(rightChildIndex)) {
+      else if (array(leftChildIndex) < array(rightChildIndex)) {
         if (swap(parentIndex, rightChildIndex))
           siftDown(rightChildIndex)
       }
@@ -78,17 +80,17 @@ object HeapTests {
       this
     }
 
-    def x(n: Int): String = {
-      if (n >= size) s"$n/error" else s"$n/${data(n)}"
+    private def prettyIndex(n: Int): String = {
+      if (n >= size) s"$n/error" else s"$n/${array(n)}"
     }
 
 
     def swap(parentIndex: Int, childIndex: Int): Boolean = {
       //println(s"  Swapping ${x(parentIndex)} - ${x(childIndex)}")
-      if (data(parentIndex) < data(childIndex)) {
-        val temp = data(parentIndex)
-        data.update(parentIndex, data(childIndex))
-        data.update(childIndex, temp)
+      if (array(parentIndex) < array(childIndex)) {
+        val temp = array(parentIndex)
+        array.update(parentIndex, array(childIndex))
+        array.update(childIndex, temp)
         true
       }
       else {
@@ -97,9 +99,9 @@ object HeapTests {
     }
 
     def isValid: Boolean = {
-      for (i <- data.length - 1 until 0 by -1) {
+      for (i <- array.length - 1 until 0 by -1) {
         val parent = (i - 1) / 2
-        if (data(i) > data(parent)) {
+        if (array(i) > array(parent)) {
           return false
         }
       }
@@ -107,19 +109,19 @@ object HeapTests {
     }
 
 
-    override def toString = s"Heap($data)"
+    override def toString = s"Heap($array)"
 
     def canEqual(other: Any): Boolean = other.isInstanceOf[Heap]
 
     override def equals(other: Any): Boolean = other match {
       case that: Heap =>
         (that canEqual this) &&
-          data == that.data
+          array == that.array
       case _ => false
     }
 
     override def hashCode(): Int = {
-      val state = Seq(data)
+      val state = Seq(array)
       state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
     }
   }
